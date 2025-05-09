9a02ad3240c1ff559046c091242bb020222733b9
Sofia-bit-2025 <24121681@student.hhs.nl>
2025-05-09 12:54:28 +0200

    JDK 17 test

---
+++ src/Test/TestJDK.java
@@ -0,0 +1,7 @@
+package Test;
+
+public class TestJDK {
+    public static void main(String[] args) {
+        System.out.println(" Dit draait met JDK " + System.getProperty("java.version"));
+    }
+}
