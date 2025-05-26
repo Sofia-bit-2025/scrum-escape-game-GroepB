22f6f60f01b526e85aa65bb6c560defed6534323
BarbaraPolano <Barbarapolano@gmail.com>
2025-05-26 11:36:34 +0200

    Create Deur.java
    mooie deur

---
+++ src/Spel/Deur.java
@@ -0,0 +1,49 @@
+package Spel;
+
+import Waarnemer.Waarnemer;
+
+public class Deur implements Waarnemer {
+    private boolean open = false;
+    private static int kamerTeller = 1;
+    private final int kamerNummer;
+
+    public Deur() {
+        this.kamerNummer = kamerTeller++;
+    }
+
+    @Override
+    public void update() {
+        open = true;
+        toonOpenDeur();
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
+
+    public void toonGeslotenDeur(){
+
+        System.out.println("+------------------------------------------------+");
+        System.out.println("|                                                |");
+        System.out.println("|                    ----------                  |");
+        System.out.println("|                    |        |                  |");
+        System.out.println("|                    |   O    |                  |");
+        System.out.println("|                    |        |                  |");
+        System.out.println("|--------------------|--------|------------------|");
+        System.out.println("\n[DEUR STATUS] De deur naar kamer " + kamerNummer + " is gesloten:\n");
+    }
+
+    public void toonOpenDeur(){
+
+        System.out.println("+------------------------------------------------+");
+        System.out.println("|                                                |");
+        System.out.println("|                    --- |------                 |");
+        System.out.println("|                    |   |     |                 |");
+        System.out.println("|                    |   |  o  |                 |");
+        System.out.println("|                    |   |     |                 |");
+        System.out.println("|--------------------|   |-----|-----------------|");
+        System.out.println("\n[DEUR STATUS] De deur naar kamer " + kamerNummer + " is open:\n");
+    }
+
+
+}
