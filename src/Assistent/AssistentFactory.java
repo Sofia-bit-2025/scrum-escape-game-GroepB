f5af0e4e0434618af1c40a0815502b530b947bda
Sofia-bit-2025 <24121681@student.hhs.nl>
2025-06-16 11:52:11 +0200

    Update AssistentFactory.java

--- src/Assistent/AssistentFactory.java
+++ src/Assistent/AssistentFactory.java
@@ -1,12 +1,15 @@
 package Assistent;
 
+import Hint.Hint;
+import Hint.HintFactory;
+
 public class AssistentFactory {
 
     public static KamerAssistent maakStandaardAssistent(String context) {
-        return new KamerAssistent(
-                context,
-                new DefaultEducatiefProvider(),
-                new DefaultMotivatieProvider()
-        );
+        EducatiefHulpmiddelProvider educatiefProvider = new DefaultEducatiefProvider();
+        MotivatieProvider motivatieProvider = new DefaultMotivatieProvider();
+        Hint hint = HintFactory.geefRandomHint(context);
+
+        return new KamerAssistent(context, educatiefProvider, motivatieProvider, hint);
     }
 }
