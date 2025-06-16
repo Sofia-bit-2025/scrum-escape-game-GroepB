bbae7efcb9570e403ff1da6c31545cf2c70eb8a1
BarbaraPolano <Barbarapolano@gmail.com>
2025-06-16 11:56:05 +0200

    Update Opstart.java

--- src/Spel/Opstart.java
+++ src/Spel/Opstart.java
@@ -39,6 +39,27 @@ public class Opstart {
         }
 
         System.out.println("Succesvol ingelogd als: " + speler.getGebruikersnaam());
+
+        if (!speler.isHeeftIntroGezien()){
+            toonIntroVerhaal();
+            speler.markeerIntroGezien();
+        }
+
         return speler;
+    }
+    private void toonIntroVerhaal() {
+        String[] verhaal = {
+                "Welkom bij Scrum Escape! \n Je zit opgesloten in een mysterieus gebouw… maar er is een uitweg.",
+                "De kamers waarin je je bevindt zijn geen gewone kamers – ze zijn gebouwd rondom de belangrijkste onderdelen van Scrum! \n Jouw missie: Doorloop alle kamers door opdrachten uit te voeren en vragen correct te beantwoorden. \n Elke kamer draait om een essentieel Scrum-element, zoals de Sprint Planning, de Daily Scrum of het Scrum Board.",
+                "Let op! Een fout antwoord roept een monster op die je moet verslaan voordat je verder kunt.",
+                "Je leert tijdens dit spel spelenderwijs: \n - Wat Scrum is \n - Hoe de verschillende onderdelen samenwerken \n - Wat jouw rol als teamlid kan zijn",
+                "Zo speel je het spel: \n Typ het commando `ga naar kamer X` om naar een kamer te gaan. \n In elke kamer wacht een uitdaging. Typ `help` om hulp te krijgen met commando’s.",
+                "Ben jij klaar om de Scrum te ontsnappen? \n Typ `start` om te beginnen..."
+        };
+
+        for (String regel : verhaal) {
+            System.out.println(regel);
+            scanner.nextLine();
+        }
     }
 }
